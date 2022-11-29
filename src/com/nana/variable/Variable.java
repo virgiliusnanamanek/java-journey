daadd14d480092c53cf812c787cb7ca2d28e5766
virgiliusnanamanek <vickynana034@gmail.com>
2022-11-29 21:45:37 +0700

    variable

---
+++ src/com/nana/variable/Variable.java
@@ -0,0 +1,11 @@
+package com.nana.variable;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		String firstName = "Vicky";
+		int age = 20;
+		System.out.println("My name is" + " " + firstName + " " + "and I am " + age + " " + "years old");
+}
+
+}
